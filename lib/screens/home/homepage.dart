6285bb335cfa94c6c4d740cde87f3e487cf5a2d8
Akash Singh <akash@bombaylabs.com>
2021-05-07 20:23:25 +0530

    remote master commit

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:saans/screens/authentication/phone_signup.dart';
+import 'package:saans/screens/home/graphs/spo2_graph.dart';
 import 'package:saans/services/auth.dart';
 import 'package:saans/services/hiveservice.dart';
 import 'package:saans/standards/global_strings.dart';
@@ -13,8 +14,11 @@ class HomePage extends StatelessWidget {
   HomePage({this.device});
   @override
   Widget build(BuildContext context) {
-    read(BluetoothCharacteristic c) async {
-      await c.setNotifyValue(!c.isNotifying);
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    void read(BluetoothCharacteristic c) async {
+      await c.setNotifyValue(
+          true); /////////////////////////////////////////////////////////////////////////////////changed
       await c.read();
     }
 
@@ -44,62 +48,85 @@ class HomePage extends StatelessWidget {
                     centerTitle: true,
                     actions: <Widget>[popupmenubotton(context)],
                   ),
-                  body: StreamBuilder<BluetoothDeviceState>(
-                    stream: device.state,
-                    builder: (context, deviceStateSnap) {
-                      return deviceStateSnap.data ==
-                              BluetoothDeviceState.connected
-                          ? StreamBuilder<List<BluetoothService>>(
-                              stream: device.services,
-                              builder:
-                                  (BuildContext context, deviceServicesSnap) {
-                                if (deviceServicesSnap.hasData &&
-                                    device.discoverServices() != null) {
-                                  return Column(
-                                    children: deviceServicesSnap.data.map((s) {
-                                      print("service uuid = ${s.uuid}");
-                                      if (s.uuid
-                                              .toString()
-                                              .toUpperCase()
-                                              .substring(4, 8) ==
-                                          '5343') {
-                                        print("FOund the required service");
-                                        return Column(
-                                          children: s.characteristics.map((c) {
-                                            print('Char');
-                                            print(c.uuid);
-                                            if (c.uuid.toString().trim() ==
-                                                "49535343-1e4d-4bd9-ba61-23c647249616") {
-                                              read(c);
-                                              print("found the char");
-                                              return StreamBuilder<List<int>>(
-                                                  stream: c.value,
-                                                  initialData: c.lastValue,
-                                                  builder: (context, valSnap) {
-                                                    final value = valSnap.data;
-                                                    print(
-                                                        "Val snap = > ${value.toString()}");
-                                                    return valSnap.hasData
-                                                        ? Text(value.toString())
-                                                        : Text("No data yet");
+                  body: Container(
+                    color: Colors.black12,
+                    alignment: Alignment.topCenter,
+                    child: SingleChildScrollView(
+                      child: StreamBuilder<BluetoothDeviceState>(
+                        stream: device.state,
+                        builder: (context, deviceStateSnap) {
+                          return deviceStateSnap.data ==
+                                  BluetoothDeviceState.connected
+                              ? StreamBuilder<List<BluetoothService>>(
+                                  stream: device.services,
+                                  builder: (BuildContext context,
+                                      deviceServicesSnap) {
+                                    if (deviceServicesSnap.hasData &&
+                                        device.discoverServices() != null) {
+                                      return Column(
+                                        children:
+                                            deviceServicesSnap.data.map((s) {
+                                          print("service uuid = ${s.uuid}");
+                                          if (s.uuid
+                                                  .toString()
+                                                  .toUpperCase()
+                                                  .substring(4, 8) ==
+                                              '5343') {
+                                            //print("FOund the required service");
+                                            return Column(
+                                              children:
+                                                  s.characteristics.map((c) {
+                                                print('Char');
+                                                print(c.uuid);
+                                                if (c.uuid.toString().trim() ==
+                                                    "49535343-1e4d-4bd9-ba61-23c647249616") {
+                                                  read(c);
+                                                  String d;
+                                                  c.value.listen((event) {
+                                                    d = event.toString();
                                                   });
-                                            } else {
-                                              print("char id dindnt match");
-                                              return Container();
-                                            }
-                                          }).toList(),
-                                        );
-                                      } else {
-                                        return Container();
-                                      }
-                                    }).toList(),
-                                  );
-                                } else {
-                                  return LoadingWidget();
-                                }
-                              })
-                          : LoadingWidget();
-                    },
+                                                  print((" LULWAAAAAAAA  $d"));
+                                                  return StreamBuilder<
+                                                          List<int>>(
+                                                      stream: c.value,
+                                                      initialData: c.lastValue,
+                                                      builder:
+                                                          (context, valSnap) {
+                                                        print(
+                                                            (" LULWAAAAAAAA2222222222  $d"));
+                                                        final value =
+                                                            valSnap.data;
+                                                        print(
+                                                            "Val snap = > ${value.toString()}");
+                                                        return valSnap.hasData
+                                                            ? uiBuilder(
+                                                                context,
+                                                                height,
+                                                                width,
+                                                                value
+                                                                    .toString())
+                                                            : Text(
+                                                                "No data yet");
+                                                      });
+                                                } else {
+                                                  print("char id dindnt match");
+                                                  return Container();
+                                                }
+                                              }).toList(),
+                                            );
+                                          } else {
+                                            return Container();
+                                          }
+                                        }).toList(),
+                                      );
+                                    } else {
+                                      return LoadingWidget();
+                                    }
+                                  })
+                              : LoadingWidget();
+                        },
+                      ),
+                    ),
                   )));
         },
       ),
@@ -124,6 +151,92 @@ PopupMenuButton popupmenubotton(BuildContext context) {
       const PopupMenuItem<String>(value: settings, child: Text(settings)),
       const PopupMenuItem<String>(value: help, child: Text(help)),
       const PopupMenuItem<String>(value: logout, child: Text(logout)),
+    ],
+  );
+}
+
+Widget uiBuilder(
+    BuildContext context, double height, double width, String data) {
+  return Column(
+    children: [
+      Container(
+        margin: EdgeInsets.only(top: width * 0.09, bottom: width * 0.09),
+        decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(6.0, 4.0), spreadRadius: 0.3, blurRadius: 10.0)
+            ],
+            borderRadius: BorderRadius.all(
+              Radius.circular(30),
+            )),
+        height: width * 0.45,
+        width: width * 0.65,
+        child: Card(
+            elevation: 15,
+            color: Colors.teal[400],
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+            child: Column(
+              children: [
+                Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: width * 0.04, horizontal: width * 0.07),
+                    child: Text("SPO2",
+                        style: GoogleFonts.raleway(
+                            textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: width * 0.05)))),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                      vertical: width * 0.009, horizontal: width * 0.05),
+                  child: Text(
+                    data.substring(0, 5),
+                    style: GoogleFonts.raleway(
+                        textStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: width * 0.17)),
+                  ),
+                )
+              ],
+            )),
+      ),
+      Container(
+        decoration: const BoxDecoration(boxShadow: [
+          BoxShadow(
+              offset: Offset(6.0, 4.0), spreadRadius: 0.3, blurRadius: 10.0)
+        ], borderRadius: BorderRadius.all(Radius.circular(30))),
+        height: width * 0.45,
+        width: width * 0.65,
+        child: Card(
+            elevation: 15,
+            color: Colors.teal[400],
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+            child: Column(
+              children: [
+                Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: width * 0.04, horizontal: width * 0.07),
+                    child: Text("Pulse Rate",
+                        style: GoogleFonts.raleway(
+                            textStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: width * 0.05)))),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                      vertical: width * 0.009, horizontal: width * 0.05),
+                  child: Text(
+                    "134",
+                    style: GoogleFonts.raleway(
+                        textStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: width * 0.17)),
+                  ),
+                )
+              ],
+            )),
+      ),
+      SPO2Graph(),
     ],
   );
 }
